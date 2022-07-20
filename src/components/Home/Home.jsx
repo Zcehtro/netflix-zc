22f7caea928607ac2bbc689c07c66eafddda5350
Zcehtro <stefano.n.giaimo@gmail.com>
2022-07-20 00:53:54 -0300

    Initial home view

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import "./Home.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function Home() {
   let page = 1;
 
   const [films, setFilms] = useState(null);
+
   useEffect(() => {
     const getFilms = async () => {
       const { data } = await axios({
@@ -18,5 +20,27 @@ export default function Home() {
     getFilms();
   }, []);
 
-  return <div className="home-main"></div>;
+  return (
+    films && (
+      <div className="home-main">
+        <div className="container-fluid film-carousel">
+          <div className="film-carousel-slide row g-0 d-flex justify-content-start align-items-center">
+            {films.map((film) => {
+              return (
+                <div className="col m-2" key={film.id}>
+                  <div className="film-div-img">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200${film.poster_path}`}
+                      alt="film-poster"
+                      className="film-poster-image"
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  );
 }
