ea43444f35c3956d1f11dca4dbc3c559ba4663b6
Zcehtro <stefano.n.giaimo@gmail.com>
2022-07-19 23:36:06 -0300

    corrections lines set up for later code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,33 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="nav-main">
-      <div className="container py-2">
-        
-          <div className="row d-flex align-items-center">
-              <div className="col-1 d-flex justify-content-start">
-                  <Link to="">
-                    <img
-                      src=".\logo192.png"
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                      alt="React logo"
-                    />
-                  </Link>
-              </div>
-              <div className="col-2 nav-box-horizontal d-flex justify-content-between">
-                <Link to="/movies" className="nav-link">
-                  Movies
-                </Link>
-                <Link to="/tv-shows" className="nav-link">
-                  TV Shows
-                </Link>
-                <Link to="/search" className="nav-link">
-                  Search
-                </Link>
-              </div>
+    <div className="navbar-main">
+      <div className="container-fluid py-2">
+        <div className="row d-flex align-items-center">
+          <div className="col-1 d-flex justify-content-center">
+            <Link to="">
+              <img
+                src=".\logo192.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React logo"
+              />
+            </Link>
           </div>
+          <div className="col-3 navbar-box-horizontal d-flex justify-content-start">
+            <Link to="/movies" className="navbar-link">
+              Movies
+            </Link>
+            <Link to="/tv-shows" className="navbar-link">
+              TV Shows
+            </Link>
+            <Link to="/search" className="navbar-link">
+              Search
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
