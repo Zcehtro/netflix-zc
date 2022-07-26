a90afa42c892fbe04e65d6af551d026e50fcfb47
Zcehtro <stefano.n.giaimo@gmail.com>
2022-07-26 01:40:49 -0300

    significant changes to film page

--- src/components/Film/Film.jsx
+++ src/components/Film/Film.jsx
@@ -2,71 +2,130 @@ import "./Film.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { intervalToDuration } from "date-fns";
 
 export default function Film() {
   const [film, setFilm] = useState(null);
-  const [filmCertRelease, setFilmCertRelease] = useState(null)
   const params = useParams();
+
+  const endpoints = [
+    `https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
+    `https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
+    `https://api.themoviedb.org/3/movie/${params.id}/external_ids?api_key=${process.env.REACT_APP_TMDB_API_KEY}`,
+    `https://api.themoviedb.org/3/movie/${params.id}/release_dates?api_key=${process.env.REACT_APP_TMDB_API_KEY}`,
+    `https://api.themoviedb.org/3/movie/${params.id}/similar?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`,
+    `https://api.themoviedb.org/3/movie/${params.id}/images?api_key=${process.env.REACT_APP_TMDB_API_KEY}&include_image_language=en`,
+  ];
 
   useEffect(() => {
     const getFilm = async () => {
       try {
-        const { data } = await axios({
-          method: "get",
-          url: `https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`,
-        });
-        console.log(20, data);
-        setFilm(data);
+        const promise = await Promise.all(
+          endpoints.map((endpoint) => {
+            return axios({ method: "get", url: endpoint });
+          })
+        );
+        const [
+          { data: details },
+          { data: credits },
+          { data: external_ids },
+          { data: release_dates },
+          { data: similar },
+          { data: images },
+        ] = promise;
+        setFilm({ details, credits, external_ids, release_dates, similar, images });
       } catch (err) {
-        console.log("film error: " & err);
-      }
-    };
-    const getFilmCertRelease = async () => {
-      try {
-        const { data } = await axios({
-          method: "get",
-          url: `https://api.themoviedb.org/3/movie/${params.id}release_dates?api_key=${process.env.REACT_APP_TMDB_API_KEY}`,
-        });
-        console.log(21, data);
-        setFilmCertRelease(data);
-      } catch (err) {
-        console.log("film error cert: " & err);
+        console.log(13, "film error: " & err);
       }
     };
     getFilm();
-    getFilmCertRelease();
   }, [params]);
 
-  const isFilmNew = (filmDateString) => {
+  const getIsFilmNew = (filmDateString) => {
     const filmDate = new Date(filmDateString);
     const nowDate = new Date();
     const diffDays = Math.ceil((nowDate - filmDate) / 1000 / 24 / 60 / 60);
 
-    console.log(3, diffDays);
     return diffDays <= 30;
   };
 
+  const getFilmReleaseYear = () => {
+    return new Date(film.details.release_date).getFullYear();
+  };
+
+  const getCertfication = () => {
+    const index1 = film.release_dates.results.findIndex((country) => country.iso_3166_1 === "US");
+    const releaseDates = film.release_dates.results[index1].release_dates;
+
+    const index2 = releaseDates.findIndex((release) => release.type >= 3);
+    if (index2 !== -1) {
+      return releaseDates[index2].certification;
+    }
+  };
+
+  const getFilmDurationFormatted = () => {
+    const durationMilliseconds = Number(film.details.runtime) * 60 * 1000;
+
+    const duration = intervalToDuration({ start: 0, end: durationMilliseconds });
+    return `${duration.hours}h ${duration.minutes}m`;
+  };
+
+  const getCastShort = () => {
+    let castShort = [];
+    film.credits.cast.map((actor) => {
+      if (castShort.length === 3) return;
+      castShort.push(actor.name);
+    });
+    return castShort.join(", ");
+  };
+
+  const getGenres = () => {
+    let genres = [];
+    film.details.genres.map((genre) => {
+      genres.push(genre.name);
+    });
+    return genres.join(", ");
+  };
+
+  console.log(20, film);
   return (
     film && (
-      <div className="film-main">
+      <div className="film-main container-fluid">
         <div className="div-film-backdrop">
           <img
             className="film-backdrop"
-            src={`https://image.tmdb.org/t/p/w500${film.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/w500${film.details.backdrop_path}`}
             alt="film-backdrop"
           />
         </div>
-        <div className="film-info-group">
-          <div className="film-info-group-left">
+        <div className="film-info-group row">
+          <div className="film-info-group-left col-8">
             <div className="film-specific-info">
-              {isFilmNew(film.release_date) ? <span className="film-is-new">New</span> : null}
-              <span>{new Date(film.release_date).getFullYear()}</span>
+              {getIsFilmNew(film.details.release_date) ? (
+                <div className="film-specific-info-item">
+                  <p className="film-is-new">New</p>
+                </div>
+              ) : null}
+              <div className="film-specific-info-item">
+                <p className="film-info-year">{getFilmReleaseYear()}</p>
+              </div>
+              <div className="film-specific-info-item">
+                <p className="film-info-certification">{getCertfication()} </p>
+              </div>
+              <div className="film-specific-info-item">
+                <p className="film-info-duration">{getFilmDurationFormatted()} </p>
+              </div>
             </div>
-            <div className="film-short-summary"></div>
+            <div className="film-short-summary">{film.details.overview}</div>
           </div>
-          <div className="film-info-group-right">
-            <div className="film-cast-short"></div>
-            <div className="film-genres"></div>
+          <div className="film-info-group-right col-4">
+            <div className="film-cast-short">
+              <span className="film-group-subtitle">Cast: </span> {getCastShort()},
+              <span style={{ fontStyle: "italic" }}> more</span>
+            </div>
+            <div className="film-genres">
+              <span className="film-group-subtitle">Genres: </span> {getGenres()}
+            </div>
             <div className="film-tags"></div>
           </div>
         </div>
