f2539847c6a8f515a38befb3ae0816f20b9e1238
Zcehtro <stefano.n.giaimo@gmail.com>
2022-08-02 22:15:16 -0300

    tv component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Movie_test from "./_tests/movie_test";
 import Home from "./components/Home/Home";
 import Movie from "./components/Movie/Movie";
 import Actor from "./components/Actor/Actor";
+import TV from "./components/TV/TV";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
         <Route path="/actor/:id" element={<Actor />} />
+        <Route path="/tv/:id" element={<TV />} />
         <Route path="/testing" element={<Movie_test />} />
       </Routes>
     </div>
